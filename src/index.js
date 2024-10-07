cc0843615e524728e57e1ce51f8f18609f4cef94
Jeferson Jimenez <jeferson.jl99@gmail.com>
2024-10-06 21:20:46 -0500

    Repair bugs

--- src/index.js
+++ src/index.js
@@ -15,5 +15,5 @@ app.set('view engine', 'ejs')
 app.use(indexRoutes)
 app.use(express.static(join(__dirname, 'public')))
 
-app.listen(process.en.PORT || 3000)
+app.listen(process.env.PORT || 3000)
 console.log('listening on port', process.en.PORT)
